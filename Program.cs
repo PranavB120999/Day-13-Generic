b30de0d8d1d89ac1c0b1f5ee31c43e6c5533c160
Pranav <pranavbhople99@gmail.com>
2022-03-28 14:31:37 +0530

    main file for Day-13-GenericProblem is added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UC_1_MaxInteger
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Greater between 3 integer program ");
+            Console.WriteLine("Enter 3 Integer Value");
+            int fst = int.Parse(Console.ReadLine());
+            int snd = int.Parse(Console.ReadLine());
+            int trd = int.Parse(Console.ReadLine());
+           
+        }
+    }
+}
