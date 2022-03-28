8dd23cfe99e51fbb0f024e6fabd1071a6dc23877
Pranav <pranavbhople99@gmail.com>
2022-03-28 14:43:08 +0530

    main file for refactor-1 is added

--- Program.cs
+++ Program.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace UC_1_MaxInteger
+namespace Refactor1
 {
     internal class Program
     {
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Greater between 3 integer program ");
-            Console.WriteLine("Enter 3 Integer Value");
-            int fst = int.Parse(Console.ReadLine());
-            int snd = int.Parse(Console.ReadLine());
-            int trd = int.Parse(Console.ReadLine());
-           
+            Console.WriteLine("Welcome to program ");
+
+            Refactor1<int> refactorint = new Refactor1<int>();
+            int max1 = refactorint.MaxValue(10, 20, 30);
+            Console.WriteLine("Maximum value of int is: " + max1);
+
+            Refactor1<double> refactorfloat = new Refactor1<double>();
+            double max2 = refactorfloat.MaxValue(10.10, 20.20, 30.30);
+            Console.WriteLine("Maximum value of float is: " + max2);
+
+            Refactor1<string> refactorstring = new Refactor1<string>();
+            string max3 = refactorstring.MaxValue("Abhi", "avi", "lol");
+            Console.WriteLine("Maximum value of string is: " + max3);
         }
     }
 }
